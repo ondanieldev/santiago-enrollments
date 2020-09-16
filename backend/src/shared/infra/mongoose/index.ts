df2c5d1cb51b30946277d52515d4d2140551e20b
Daniel Oliveira <contato@ondaniel.com.br>
2020-09-16 16:46:55 -0300

    on-line cluester added with dotenv

--- backend/src/shared/infra/mongoose/index.ts
+++ backend/src/shared/infra/mongoose/index.ts
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
 mongoose
-    .connect('mongodb://localhost:27017/santiago', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .catch(() => {
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@santiago.lorra.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+    )
+    .catch(err => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!';
+        console.log(err);
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
     });
