1cf5f51a7d173c8cc80067f127ae53e0cb5c60a3
Daniel Oliveira <contato@ondaniel.com.br>
2020-09-17 10:55:45 -0300

    mongodb test

--- backend/src/shared/infra/mongoose/index.ts
+++ backend/src/shared/infra/mongoose/index.ts
@@ -11,5 +11,8 @@ mongoose
     .catch(err => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!';
         console.log(err);
+        console.log(
+            `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@santiago.lorra.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+        );
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
     });
