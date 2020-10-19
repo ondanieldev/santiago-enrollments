b6323a99627ba1bd354b6c9ec191a01be41946e3
Daniel Oliveira <contato@ondaniel.com.br>
2020-10-19 11:20:34 -0300

    Updating table data

--- admin-frontend/src/pages/EnrollmentsTable/index.tsx
+++ admin-frontend/src/pages/EnrollmentsTable/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { format as formatDate, parseISO } from 'date-fns';
 
 import { Container, List } from './styles';
 import IconBar from '../../components/IconBar';
@@ -33,31 +34,70 @@ const EnrolmentsTable: React.FC = () => {
     <Container>
       <IconBar />
 
-      <List>
-        <thead>
-          <tr>
-            <td>Turma</td>
-            <td>Nome</td>
-            <td>Responsável financeiro</td>
-            <td>R.F. Número</td>
-            <td>Responsável Solidário</td>
-            <td>R.S. Número</td>
-          </tr>
-        </thead>
-
-        <tbody>
-          {reenrollments.map(reenrollment => (
-            <tr key={reenrollment.enrollment_number}>
-              <td>{reenrollment.formated_grade}</td>
-              <td>{reenrollment.student_name}</td>
-              <td>{reenrollment.financial_name}</td>
-              <td>{reenrollment.financial_personal_phone}</td>
-              <td>{reenrollment.supportive_name}</td>
-              <td>{reenrollment.supportive_personal_phone}</td>
+      <div>
+        <List>
+          <thead>
+            <tr>
+              <td colSpan={1}>Turma</td>
+              <td colSpan={5}>Aluno</td>
+              <td colSpan={5}>Responsável financeiro</td>
+              <td colSpan={5}>Responsável solidário</td>
+              <td colSpan={1}>Mãe</td>
+              <td colSpan={1}>Pai</td>
             </tr>
-          ))}
-        </tbody>
-      </List>
+            <tr>
+              <td>Nome</td>
+              <td>Nome</td>
+              <td>Situação</td>
+              <td>Data da matrícula</td>
+              <td>Data de nascimento</td>
+              <td>Natrualidade</td>
+              <td>Nome</td>
+              <td>Parentesco</td>
+              <td>Celular</td>
+              <td>CPF</td>
+              <td>E-mail</td>
+              <td>Nome</td>
+              <td>Parentesco</td>
+              <td>Celular</td>
+              <td>CPF</td>
+              <td>E-mail</td>
+              <td>Nome</td>
+              <td>Nome</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {reenrollments.map(reenrollment => (
+              <tr key={reenrollment.enrollment_number}>
+                <td>{reenrollment.formated_grade}</td>
+                <td>{reenrollment.student_name}</td>
+                <td>{reenrollment.type === 'reenrollment' ? 'R' : 'N'}</td>
+                <td> - </td>
+                <td>
+                  {formatDate(
+                    parseISO(reenrollment.student_birth_date.toString()),
+                    'dd/MM/yyyy',
+                  )}
+                </td>
+                <td>{`${reenrollment.student_birth_city} - ${reenrollment.student_birth_state}`}</td>
+                <td>{reenrollment.financial_name}</td>
+                <td>{reenrollment.financial_kinship}</td>
+                <td>{reenrollment.financial_personal_phone}</td>
+                <td>{reenrollment.financial_cpf}</td>
+                <td>{reenrollment.financial_email}</td>
+                <td>{reenrollment.supportive_name}</td>
+                <td>{reenrollment.supportive_kinship}</td>
+                <td>{reenrollment.supportive_personal_phone}</td>
+                <td>{reenrollment.supportive_cpf}</td>
+                <td>{reenrollment.supportive_email}</td>
+                <td>{reenrollment.student_mother_name}</td>
+                <td>{reenrollment.student_father_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </List>
+      </div>
     </Container>
   );
 };
--- admin-frontend/src/pages/EnrollmentsTable/styles.ts
+++ admin-frontend/src/pages/EnrollmentsTable/styles.ts
@@ -1,27 +1,31 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   width: 100%;
   padding: 24px;
+
+  > div {
+    width: 100%;
+    overflow-x: scroll;
+  }
 `;
 
 export const List = styled.table`
-  width: 100%;
-  overflow-x: scroll;
   border: 1px solid #212529;
   border-collapse: collapse;
-
-  th,
-  td {
-    border: 1px solid #212529;
-    padding: 12px;
-  }
+  text-align: center;
 
   tr {
     transition: 0.1s all linear;
+
+    td {
+      padding: 12px;
+      white-space: nowrap;
+    }
+
+    > td {
+      border: 1px solid #212529;
+    }
 
     &:hover {
       background-color: #ffcf00;
@@ -31,6 +35,12 @@ export const List = styled.table`
   thead {
     tr {
       font-weight: bold;
+      background-color: #013c64;
+      color: #f7f6fc;
+
+      &:hover {
+        background-color: #013c64;
+      }
     }
   }
 `;
