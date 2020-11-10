1a116af48cb6041ad6b31cd7d46cfcd87443cc3e
stemDaniel <contato@ondaniel.com.br>
2020-11-10 15:06:49 +0000

    Updating mongodb URI format

--- backend/src/shared/infra/mongoose/index.ts
+++ backend/src/shared/infra/mongoose/index.ts
@@ -1,14 +1,8 @@
 import mongoose from 'mongoose';
 
-const user = process.env.MONGO_USER;
-const pass = process.env.MONGO_PASSWORD;
-const host = process.env.MONGO_HOST;
-const port = process.env.MONGO_PORT ? `:${process.env.MONGO_PORT}` : '';
-const dtbs = process.env.MONGO_DATABASE;
-
 mongoose
     .connect(
-        `mongodb+srv://${user}:${pass}@${host}${port}/${dtbs}?retryWrites=true&w=majority`,
+        process.env.MONGODB_URI || '',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
